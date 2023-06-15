5c68cac3dca3dda326576b4bee73bc6bfa49b4d7
dev5 <dev5@ccript.com>
2023-06-15 17:53:53 +0500

    add loading indicator between routes

---
+++ src/app/(buyer)/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from "@/common/Loader";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-full grid place-content-center">
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
