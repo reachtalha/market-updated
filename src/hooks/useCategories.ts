633894906bbf109f5f72be8d8ef9dbbddd22c522
dev5 <dev5@ccript.com>
2023-07-04 19:20:53 +0500

    create custom hook for categories data

---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import useSWR from "swr";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase/client";
+
+export interface ICategory {
+  id: string;
+  image: string;
+  name: string;
+  lifespan: string;
+}
+
+export const useCategories = () => {
+  const categoriesRef = collection(db, "categories");
+
+  const fetcher = async (): Promise<ICategory[]> => {
+    const querySnapshot = await getDocs(categoriesRef);
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as ICategory[];
+  };
+
+  const { data, error, isLoading } = useSWR<ICategory[]>("categories", fetcher);
+
+  return {
+    categories: data,
+    isError: error,
+    isLoading,
+  };
+};
