f2c4da9a0fe8c7fd097a8dcb342d8b0db1c79a3d
talhacdev <thecorruptmob@gmail.com>
2023-07-26 19:37:31 +0500

    created coming soon page

--- src/app/(buyer)/experts/[expertId]/page.tsx
+++
@@ -1,5 +0,0 @@
-const Expert = ({ params }: { params: { expertId: string } }) => {
-  return <div>Expert {params.expertId}</div>;
-};
-
-export default Expert;
--- src/assets/icons/system/CircledArrowRight.tsx
+++ src/assets/icons/system/CircledArrowRight.tsx
@@ -1,28 +1,9 @@
-export default function CircledArrowRight({ ...props }) {
+export default function CircledArrowRight({ ...props }){
   return (
-    <svg
-      {...props}
-      width="60"
-      height="60"
-      viewBox="0 0 60 60"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        x="1"
-        y="1"
-        width="58"
-        height="58"
-        rx="29"
-        stroke="white"
-        strokeWidth="2"
-      />
-      <path
-        d="M25 40L36 30L25 20"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="square"
-      />
+    <svg {...props} width="60" height="60" viewBox="0 0 60 60" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <rect x="1" y="1" width="58" height="58" rx="29" stroke="white" strokeWidth="2"/>
+      <path d="M25 40L36 30L25 20" stroke="white" strokeWidth="2" strokeLinecap="square"/>
     </svg>
-  );
-}
+  )
+};
+
