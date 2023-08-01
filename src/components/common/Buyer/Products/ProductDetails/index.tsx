3b25ef3dba94e2d8ff97ec2e67a1623e79613fcb
Said Dawood <said.dawood@spqr.agency>
2023-08-01 20:41:59 +0500

    product details and searchbar

--- src/components/common/Buyer/Products/ProductDetails/index.tsx
+++ src/components/common/Buyer/Products/ProductDetails/index.tsx
@@ -1,12 +1,16 @@
 "use client"
-import ProductSlider from '@/components/common/Buyer/Products/ProductDetails/ProductSlider';
 import Link from 'next/link';
-import SelectProductVariant from '@/components/common/Buyer/Products/ProductDetails/SelectProductVariant';
-import DetailsAccordion from '@/components/common/Buyer/Products/ProductDetails/DetailsAccordion';
 import Stars from '@/assets/icons/system/Stars';
 import { Button } from '@/components/ui/button';
+
+import ProductSlider from '@/components/common/Buyer/Products/ProductDetails/ProductSlider';
+import SelectProductVariant from '@/components/common/Buyer/Products/ProductDetails/SelectProductVariant';
+import DetailsAccordion from '@/components/common/Buyer/Products/ProductDetails/DetailsAccordion';
 import ProductVideo from '@/components/common/Buyer/Products/ProductDetails/ProductVideo';
 import ProductReviews from '@/components/common/Buyer/Products/ProductDetails/ProductReviews';
+import ComplementaryProducts from '@/components/common/Buyer/Products/ProductDetails/ComplementaryProducts';
+import BlogCard from '@/components/common/Buyer/Products/ProductDetails/BlogCard';
+
 
 const product = {
   name: 'Body Wash',
@@ -33,11 +37,11 @@ const product = {
 export default function Product(){
   return (
       <>
-        <div className="grid items-center grid-cols-3 gap-x-16">
+        <div className="grid items-center gap-y-8 grid-cols-1 md:grid-cols-3 md:gap-x-16">
           <div className="col-span-2 h-full">
             <ProductSlider />
           </div>
-         <div className="col-span-1">
+         <div className="w-full md:col-span-1">
            <h1 className="font-regular border-b-2 pb-4 border-black uppercase">{product.name}</h1>
            <span className="block font-sm mt-1 mb-6">15.2   FL   OZ   /   450   ML</span>
            <p className="font-medium">{product.description}</p>
@@ -60,6 +64,16 @@ export default function Product(){
 
            <Button className="w-full mt-14 uppercase">Add to bag</Button>
          </div>
+
+        </div>
+        <div className="grid items-center gap-y-8 grid-cols-1 lg:grid-cols-3 md:gap-x-16">
+          <div className="order-2 md:order-1 gap-y-8 flex flex-col lg:flex-row md:gap-x-10 mt-16 h-full col-span-2">
+            <BlogCard />
+            <BlogCard />
+          </div>
+          <div className="order-1 md:order-2 col-span-1">
+            <ComplementaryProducts />
+          </div>
         </div>
         <ProductVideo />
         <ProductReviews />
---
+++ src/components/common/Searchbar.tsx
@@ -0,0 +1,67 @@
+import { useRef } from 'react';
+import Link from 'next/link';
+import { ArrowRight, SearchIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+import { Input } from '@/components/ui/input';
+import BoxedContent from '@/components/common/BoxedContent';
+
+import useClickOutside from '@/hooks/useClickOutside';
+
+type SearchbarProps = {
+  isOpen: boolean
+  toggleSearchBar: Function
+}
+
+const quickLinks = [
+  {
+    id: 1,
+    name: 'BODY WASH',
+    link: '/products/123'
+  },
+  {
+    id: 2,
+    name: 'BODY WASH',
+    link: '/products/123'
+  },
+  {
+    id: 3,
+    name: 'BODY WASH',
+    link: '/products/123'
+  },
+  {
+    id: 4,
+    name: 'BODY WASH',
+    link: '/products/123'
+  },
+]
+export default function Searchbar({ isOpen, toggleSearchBar }: SearchbarProps){
+  const searchBoxRef = useRef();
+  useClickOutside(searchBoxRef, toggleSearchBar);
+  return (
+   <div
+     ref={searchBoxRef}
+     tabIndex={0}
+     className={cn("invisible shadow shadow-gray-200 absolute h-0 transition-all ease-in duration-100 w-full bg-white text-black", isOpen && "visible h-[400px]")}
+   >
+     <BoxedContent className="mt-16">
+       <form className="pt-6">
+         <div className="flex gap-2 items-center">
+           <SearchIcon height={25} width={25}/>
+           <Input autoFocus className="bg-transparent placeholder:text-black focus-visible:ring-0 border-0 outline-0 text-2xl" placeholder="Search all Organic" />
+         </div>
+       </form>
+
+       <h3 className="uppercase text-gray-500 mt-4">Quick Links</h3>
+       <ul className="flex flex-col gap-y-4 mt-4">
+         {quickLinks.map(item => <li className="" key={item.id}>
+           <Link onClick={toggleSearchBar} className="flex items-center gap-4 hover:underline" href={item.link}>
+             <ArrowRight className="text-gray-500" />
+             {item.name}
+           </Link>
+         </li>)}
+       </ul>
+     </BoxedContent>
+   </div>
+ )
+}
