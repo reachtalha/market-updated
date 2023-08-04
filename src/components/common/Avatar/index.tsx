1f6c88678e774e2e5713f737c04415cac6e34c8f
dev5 <dev5@ccript.com>
2023-08-04 17:36:49 +0500

    refactor: updated avatar component

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -9,13 +9,15 @@ const Avatar = ({ name, photoURL }: Props) => {
   return (
     <>
       {photoURL ? (
-        <Image
-          width={40}
-          height={40}
-          src={photoURL}
-          alt=""
-          className="h-10 w-10 rounded-full object-cover"
-        />
+        <div className="p-0.5 border-2 border-neutral-300 rounded-full overflow-hidden">
+          <Image
+            width={40}
+            height={40}
+            src={photoURL}
+            alt=""
+            className="h-10 w-10 rounded-full object-cover"
+          />
+        </div>
       ) : (
         <button>
           <Avvvatars size={40} value={name!} />
