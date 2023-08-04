bd44920686404b22ad4ed005decad6f598a46498
dev5 <dev5@ccript.com>
2023-08-04 18:45:27 +0500

    refactor: made borders rounded

--- src/components/common/Buyer/Products/ProductDetails/ProductSlider.tsx
+++ src/components/common/Buyer/Products/ProductDetails/ProductSlider.tsx
@@ -16,7 +16,7 @@ export default function ProductSlider({ images }: { images: string[] }) {
     >
       {images.map((url: string, index: number) => (
         <SwiperSlide className=" w-full h-full" key={index}>
-          <div className="relative w-full h-[60vh] md:h-full">
+          <div className="relative w-full rounded-lg overflow-hidden h-[60vh] md:h-full">
             <Image
               sizes="100%"
               className="w-full  h-full object-cover object-center"
