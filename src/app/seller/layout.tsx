1d61d1d550e81b833387d6c3175ac28f0de207bb
dev5 <dev5@ccript.com>
2023-08-04 17:38:41 +0500

    refactor: improved redirecting

--- src/app/seller/layout.tsx
+++ src/app/seller/layout.tsx
@@ -1,25 +1,23 @@
-'use client';
-import React, { useEffect } from 'react';
-import Sidebar from '@/components/common/Seller/Sidebar';
-import MobileNavbar from '@/components/common/Seller/Navbar/MobileNavbar';
+"use client";
+
+import { useRouter } from 'next/navigation';
+
 import useGlobalStore from '@/state';
 import { useRole } from '@/hooks/useUserRole';
-import { useRouter } from 'next/navigation';
+
+import Sidebar from '@/components/common/Seller/Sidebar';
+import MobileNavbar from '@/components/common/Seller/Navbar/MobileNavbar';
+import Loader from '@/components/common/Loader';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { showSidebar } = useGlobalStore() as any;
   const role = useRole();
   const router = useRouter();
 
-  useEffect(() => {
-    console.log('here', role);
-    if (role === 'loading') {
-      return;
-    }
-    if (role !== 'seller') {
-      router.push('/auth/register');
-    }
-  }, [role, router]);
+  if (!role || role !== "seller") {
+    router.replace('/');
+    return <Loader className="bg-white h-screen w-full overflow-hidden grid place-content-center" />
+  }
 
   return (
     <div className={`bg-neutral-900 flex  h-screen overflow-hidden  sm:p-1.5 sm:gap-1.5 `}>
@@ -27,9 +25,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <Sidebar />
       </aside>
       <section
-        className={`bg-neutral-50  transition-all  duration-1000 overflow-scroll no-scrollbar   text-neutral-900  flex-1 sm:rounded-xl  ${
-          showSidebar && 'tilted-div rounded-xl'
-        }`}
+        className={`bg-neutral-50  transition-all  duration-1000 overflow-scroll no-scrollbar   text-neutral-900  flex-1 sm:rounded-xl  ${showSidebar && 'tilted-div rounded-xl'
+          }`}
       >
         <MobileNavbar />
 
