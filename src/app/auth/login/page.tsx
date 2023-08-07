383cc1791f51aee3f93768392ad0c82755c04a60
dev5 <dev5@ccript.com>
2023-08-07 19:30:49 +0500

    fix: make UI changes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -9,6 +9,9 @@ import Google from '@/assets/icons/social/Google';
 import Facebook from '@/assets/icons/social/Facebook';
 
 import toast from 'react-hot-toast';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface LoginForm {
   email: string;
@@ -23,7 +26,6 @@ const Login = () => {
     signInWithFacebookAccount,
     sessionBasedSignin,
     loading,
-    error
   } = useAuth();
 
   const {
@@ -38,9 +40,6 @@ const Login = () => {
     } else {
       signIn(email, password);
     }
-    if (error) {
-      toast.error(`Error occured! ${error}`);
-    }
   };
   return (
     <>
@@ -53,10 +52,10 @@ const Login = () => {
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-3 my-4">
           <div className="space-y-1">
-            <label className="text-sm">Email</label>
-            <input
+            <Label>Email</Label>
+            <Input
               type="email"
-              className="w-full rounded-xl border-[2px] border-gray-300 p-2 transition-transform delay-75 duration-300 placeholder:text-sm hover:border-killarney-700 focus:-translate-y-[2px] focus:outline-killarney-700"
+              className="w-full placeholder:text-sm"
               placeholder="Your email"
               {...register('email', {
                 required: true,
@@ -68,10 +67,10 @@ const Login = () => {
             )}
           </div>
           <div className="space-y-1">
-            <label className="text-sm">Password</label>
-            <input
+            <Label>Password</Label>
+            <Input
               type="password"
-              className="w-full rounded-xl border-[2px] border-gray-300 p-2 transition-transform delay-75 duration-300 placeholder:text-sm hover:border-killarney-700 focus:-translate-y-[2px] focus:outline-killarney-700"
+              className="w-full placeholder:text-sm"
               placeholder="Your password"
               {...register('password', {
                 required: true
@@ -90,40 +89,40 @@ const Login = () => {
               Forgot Password?
             </Link>
           </div>
-          <button
+          <Button
             type="submit"
             disabled={loading}
-            className="disabled:cursor-not-allowed w-full border bg-primary opacity-90 hover:opacity-100 duration-300 transition-opacity focus: outline text-white rounded py-2 m-0"
+            variant="default"
+            className="w-full"
           >
             Login
-          </button>
+          </Button>
         </form>
         <p className="my-2 text-primary text-center">
           <b>OR</b>
         </p>
         <div className="flex justify-center gap-x-3">
-          <div className="w-36 flex items-center cursor-pointer justify-center  p-1 rounded-full bg-gray-100 hover:bg-gray-200 transition-colors duration-300">
-            <button
-              type="button"
-              disabled={loading}
-              onClick={signInWithGoogleAccount}
-              className="disabled:cursor-not-allowed mr-auto bg-gray-200  rounded-full border p-2"
+          <Button type="button"
+            disabled={loading}
+            onClick={signInWithGoogleAccount} className="w-36 h-fit gap-x-2.5 group flex items-center cursor-pointer p-1 rounded-full bg-gray-100  hover:bg-primary transition-colors duration-300">
+            <span
+              className="bg-gray-200  rounded-full border p-2"
             >
               <Google className="w-5 2xl:w-8 h-5 2xl:h-8" fill={'#414D35'} />
-            </button>
-            <span className="mr-auto">Google</span>
-          </div>
-          <div className="w-36 flex items-center cursor-pointer justify-center p-1 rounded-full bg-primary opacity-90 hover:opacity-100 transition-opacity duration-300 ">
-            <button
-              type="button"
-              disabled={loading}
-              onClick={signInWithFacebookAccount}
-              className="disabled:cursor-not-allowed mr-auto bg-gray-200  rounded-full border p-2"
+            </span>
+            <span className="mr-auto text-primary group-hover:text-white">Google</span>
+          </Button>
+          <Button type="button"
+            disabled={loading}
+            onClick={signInWithFacebookAccount}
+            className="w-36 h-fit group flex items-center cursor-pointer gap-x-2.5 p-1 rounded-full bg-gray-100  hover:bg-primary transition-colors duration-300"            >
+            <span
+              className=" bg-gray-200  rounded-full border p-2"
             >
-              <Facebook className="w-5 2xl:w-8 h-5 2xl:h-8" fill={'#414D35'} />
-            </button>
-            <span className="mr-auto text-white">Facebook</span>
-          </div>
+              <Facebook className="w-5 2xl:w-8 h-5 2xl:h-8 " fill={'#414D35'} />
+            </span>
+            <span className="mr-auto text-primary group-hover:text-white">Facebook</span>
+          </Button>
         </div>
         <p className="text-center mt-3">
           Not a member yet?{' '}
