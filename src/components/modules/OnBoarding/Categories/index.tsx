dbfac3d4a7045575b50695ad2fd1b852d8a21900
dev5 <dev5@ccript.com>
2023-08-17 15:19:58 +0500

    fix interface fields

--- src/components/modules/OnBoarding/Categories/index.tsx
+++ src/components/modules/OnBoarding/Categories/index.tsx
@@ -5,7 +5,6 @@ import { useCategories, ICategory } from '@/hooks/useCategories';
 import { useFormContext } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
 
 import Loader from '@/components/common/Loader';
 import Error from '@/components/common/Error';
@@ -41,7 +40,6 @@ const Categories = () => {
   if (isError) {
     return <Error className="py-16 grid place-content-center h-full w-full" />;
   }
-
   return (
     <>
       <div className="mb-3">
@@ -53,13 +51,13 @@ const Categories = () => {
           <Label key={c.id}>
             <Input
               type="checkbox"
-              value={c.name}
-              onChange={() => select(c.name)}
-              checked={selected.includes(c.name) ? true : false}
+              value={c.title}
+              onChange={() => select(c.title)}
+              checked={selected.includes(c.title) ? true : false}
               className="peer hidden"
             />
-            <div className="w-fit rounded-full border-2 border-gray-300 px-3 py-2 text-sm capitalize peer-checked:border-neutral-800 peer-checked:bg-neutral-800 peer-checked:text-white 2xl:text-base">
-              {c.name}
+            <div className="w-fit rounded-full border-2 border-gray-300 px-3 py-2 text-sm capitalize peer-checked:border-primary peer-checked:bg-primary peer-checked:text-white 2xl:text-base">
+              {c.title}
             </div>
           </Label>
         ))}
