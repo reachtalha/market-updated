5cd45ba725e33d34bae339fe9ec7ab82ef1cb1c6
dev5 <dev5@ccript.com>
2023-08-17 15:20:28 +0500

    fix button styling

--- src/components/modules/chat/Room/Input.tsx
+++ src/components/modules/chat/Room/Input.tsx
@@ -15,13 +15,10 @@ import { toast } from 'react-hot-toast';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
-import { useSWRConfig } from 'swr';
 
 import { ImagePlus, Send } from 'lucide-react';
 
 const InputBox = ({ chatId }: { chatId: string }) => {
-  const { mutate } = useSWRConfig();
-
   const imageRef = useRef(null);
 
   const addMessage = async (message: string, type: string) => {
@@ -33,8 +30,6 @@ const InputBox = ({ chatId }: { chatId: string }) => {
         sentBy: auth.currentUser?.uid,
         type: type
       });
-      // mutate(`${chatId}-chatroom`);
-      // mutate(`${chatId}-messages`);
     } catch (error) {
       toast.error('Oops! Something went wrong!');
     }
@@ -78,7 +73,7 @@ const InputBox = ({ chatId }: { chatId: string }) => {
   };
   return (
     <form onSubmit={sendTextMessage}>
-      <div className="flex w-full items-center justify-between border-t border-gray-300 px-1 py-3 md:p-3">
+      <div className="flex w-full items-center justify-between border-t border-neutral-300 px-1 py-3 md:p-3">
         <Input
           name="textMessage"
           type="text"
@@ -86,7 +81,7 @@ const InputBox = ({ chatId }: { chatId: string }) => {
           pattern="^\s*\S.*$"
           autoComplete="off"
           placeholder="Type message here..."
-          className="mx-1.5 block w-full rounded-full border bg-gray-100 py-2 pl-4 outline-none drop-shadow-sm focus:text-gray-700 md:mx-3"
+          className="mx-1.5 block w-full rounded-full border bg-neutral-100 py-2 pl-4 outline-none drop-shadow-sm focus:text-neutral-700 md:mx-3"
           required
           onInvalid={(e: any) =>
             (e.target as HTMLInputElement).setCustomValidity(
@@ -98,7 +93,7 @@ const InputBox = ({ chatId }: { chatId: string }) => {
         <div className="mr-2">
           <Label htmlFor="file-input">
             <ImagePlus
-              className="h-6 w-6 cursor-pointer text-gray-500 hover:text-gray-700"
+              className="h-6 w-6 cursor-pointer text-primary/90 hover:text-primary"
               strokeWidth={1.5}
             />
           </Label>
@@ -114,9 +109,10 @@ const InputBox = ({ chatId }: { chatId: string }) => {
         </div>
         <Button
           type="submit"
-          className="rounded-full bg-killarney-100 p-2.5 hover:bg-killarney-200"
+          variant="default"
+          className="rounded-full px-2.5 py-1.5"
         >
-          <Send className="h-6 w-6 text-primary" strokeWidth={1.5} />
+          <Send className="h-5 w-5 text-white" strokeWidth={1.5} />
         </Button>
       </div>
     </form>
