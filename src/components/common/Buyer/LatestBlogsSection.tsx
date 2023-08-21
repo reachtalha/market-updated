cd77bc6e2a0da2cae0afbf10c5f91208fc884ed0
Said Dawood <said.dawood@spqr.agency>
2023-08-21 14:51:32 +0500

    remove contentlayer

--- src/components/common/Buyer/LatestBlogsSection.tsx
+++ src/components/common/Buyer/LatestBlogsSection.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link';
-import { allPosts, Post } from 'contentlayer/generated'
 import { format, parseISO } from 'date-fns';
 
 import BoxedContent from '@/components/common/BoxedContent';
 import { cn } from '@/lib/utils';
 
-function PostCard(post: Post) {
+function PostCard() {
+  const post = {
+    url: "",
+    title: "",
+    date: new Date()
+  }
   return (
     <Link href={post.url} className="p-3 w-full h-[200px] md:h-[350px] grid place-content-center rounded-lg object-cover bg-gray-100">
       <h2 className="font-medium text-primary text-2xl mb-1">
         {post.title}
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
+      <time dateTime={post.date.toDateString()} className="mb-2 block text-xs text-gray-600">
       </time>
     </Link>
   )
@@ -31,9 +34,6 @@ export default function LatestBlogsSection({ title, className = '' }: LatestBlog
         <h3 className="text-lg md:text-3xl">@allorganicsmarket</h3>
       </header>
       <div className="flex flex-col md:flex-row justify-between gap-y-4 md:gap-x-4">
-        {allPosts.map((post: Post, idx: number) => (
-          <PostCard key={idx} {...post} />
-        ))}
       </div>
     </BoxedContent>
   );
