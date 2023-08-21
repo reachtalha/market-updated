3cb9cd0be999533e38506e5dbaf53f6ce3edfbae
dev5 <dev5@ccript.com>
2023-08-21 13:06:51 +0500

    add dashboard analytics

--- src/app/seller/dashboard/page.tsx
+++ src/app/seller/dashboard/page.tsx
@@ -7,16 +7,18 @@ import { getDocs, query, collection, where, limit } from "@firebase/firestore";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { RecentSales } from '@/components/modules/Seller/Dashboard/RecentSales';
 import Overview from '@/components/modules/Seller/Dashboard/Overview';
 
-import { formatDate } from '@/utils/formatters';
+import { formatCurrency, formatDate, formatMonth } from '@/utils/formatters';
 import Loader from '@/components/common/Loader';
 import Error from '@/components/common/Error';
+
+import Products from '@/assets/icons/system/Products';
+import Cart from '@/assets/icons/system/Cart';
 
 
 export default function Home() {
@@ -30,36 +32,40 @@ export default function Home() {
     let totalSales = 0;
     let totalProducts = 0;
     let totalOrders = 0;
-    const productQuery = getDocs(
+    const productSnap = await getDocs(
       query(
         collection(db, "shops"),
         where("uid", "==", `${auth.currentUser?.uid}`),
         limit(1)
       )
     );
-    const orderQuery = getDocs(
+    const shopId = productSnap.docs[0]?.id;
+    const orderSnap = await getDocs(
       query(
         collection(db, "orders"),
-        where("sellers", "array-contains", `${auth.currentUser?.uid}`)
+        where("shops", "array-contains", `${shopId}`)
       )
     );
-    const [productSnap, orderSnap] = await Promise.all([
-      productQuery,
-      orderQuery,
-    ]);
+
     totalProducts = productSnap.docs[0]?.data()?.noOfProducts;
+    let activeOrders = 0;
     orderSnap.forEach((order) => {
       totalOrders++;
+      if (order.data().status === "processing") activeOrders++;
       recentSales.push({
         id: order.id,
-        name: order.data().name,
+        firstName: order.data().shippingAddress.firstName,
+        lastName: order.data().shippingAddress.lastName,
+        email: order.data().shippingAddress.email ?? "",
+        photoURL: order.data().photoURL ?? "",
         amount: order.data().total,
-        time: formatDate(order.data().placedAt.seconds * 1000),
-        status: order.data().status,
+        time: formatDate(order.data().timeStamp.seconds * 1000),
+        month: formatMonth(order.data().timeStamp.seconds * 1000),
+        status: order.data().status ?? "Processing",
       });
-      order.data().products.map((item: any) => {
-        if (item.uid === auth.currentUser?.uid) {
-          totalSales += parseInt(item.price);
+      order.data().items.map((item: any) => {
+        if (item.shopId === shopId) {
+          totalSales += parseInt(item.selectedVariant.price);
         }
       });
     });
@@ -68,7 +74,8 @@ export default function Home() {
       totalProducts,
       totalOrders,
       recentSales,
-    };
+      activeOrders,
+    } as any;
   });
   if (isLoading) {
     return <Loader className="h-full w-full grid place-content-center" />;
@@ -76,7 +83,6 @@ export default function Home() {
   if (error) {
     return <Error className="h-full w-full grid place-content-center" />;
   }
-  console.log(analytics)
 
   return (
     <main className="p-3 md:p-5 lg:p-8 space-y-4">
@@ -95,73 +101,39 @@ export default function Home() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
+              className="h-5 w-5 text-neutral-600"
             >
               <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
             </svg>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +20.1% from last month
-            </p>
+            <div className="text-2xl font-bold">{formatCurrency(analytics.totalSales)}</div>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">
-              Subscriptions
+              Total Products
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
+            <Products className="w-5 h-5 text-neutral-600" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
-            <p className="text-xs text-muted-foreground">
-              +180.1% from last month
-            </p>
+            <div className="text-2xl font-bold">{analytics.totalProducts}</div>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
+            <CardTitle className="text-sm font-medium">Total Orders</CardTitle>
+            <Cart className="w-5 h-5 text-neutral-600" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">
-              +19% from last month
-            </p>
+            <div className="text-2xl font-bold">{analytics.totalOrders}</div>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">
-              Active Now
+              Active Orders
             </CardTitle>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -177,33 +149,13 @@ export default function Home() {
             </svg>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+573</div>
-            <p className="text-xs text-muted-foreground">
-              +201 since last hour
-            </p>
+            <div className="text-2xl font-bold">{analytics.activeOrders}</div>
           </CardContent>
         </Card>
       </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-        <Card className="col-span-4">
-          <CardHeader>
-            <CardTitle>Overview</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <Overview />
-          </CardContent>
-        </Card>
-        <Card className="col-span-3">
-          <CardHeader>
-            <CardTitle>Recent Sales</CardTitle>
-            <CardDescription>
-              You made 265 sales this month.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <RecentSales />
-          </CardContent>
-        </Card>
+        <Overview sales={analytics.recentSales} />
+        <RecentSales sales={analytics.recentSales} />
       </div>
     </main>
   );
