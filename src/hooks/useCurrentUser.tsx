704d121047a98689aeb270a8197b5e3828c1ec3f
dev5 <dev5@ccript.com>
2023-08-24 14:32:18 +0500

    remove dependency

--- src/hooks/useCurrentUser.tsx
+++ src/hooks/useCurrentUser.tsx
@@ -1,36 +1,39 @@
-"use client";
+'use client';
 
 import { onSnapshot, doc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { db, auth } from '@/lib/firebase/client';
 
 export const useCurrentUser = () => {
-    const [user, setUser] = useState<any>(null);
-    const [loading, setLoading] = useState<boolean>(false);
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    useEffect(() => {
-        setLoading(true);
-        const docRef = doc(db, "users", `${auth.currentUser?.uid}`);
-        const unsubscribe = onSnapshot(docRef, (docSnap) => {
-            if (docSnap.exists()) {
-                setUser({ id: docSnap.id, ...docSnap.data() });
-            } else {
-                setUser(null);
-            }
-            setLoading(false);
-        }, (error) => {
-            console.error("Error fetching user data:", error);
-            setUser(null);
-            setLoading(false);
-        });
+  useEffect(() => {
+    setLoading(true);
+    const docRef = doc(db, 'users', `${auth.currentUser?.uid}`);
+    const unsubscribe = onSnapshot(
+      docRef,
+      (docSnap) => {
+        if (docSnap.exists()) {
+          setUser({ id: docSnap.id, ...docSnap.data() });
+        } else {
+          setUser(null);
+        }
+        setLoading(false);
+      },
+      (error) => {
+        setUser(null);
+        setLoading(false);
+      }
+    );
 
-        return () => {
-            unsubscribe();
-        };
-    }, [auth.currentUser]);
-
-    return {
-        user,
-        isLoading: loading
+    return () => {
+      unsubscribe();
     };
+  }, []);
+
+  return {
+    user,
+    isLoading: loading
+  };
 };
