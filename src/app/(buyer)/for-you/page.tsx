3bae85b91b8342f24c4968c12a35724d127efaac
dev5 <dev5@ccript.com>
2023-08-24 16:50:57 +0500

    refactor: add code splits, improve water fall

--- src/app/(buyer)/for-you/page.tsx
+++ src/app/(buyer)/for-you/page.tsx
@@ -1,63 +1,31 @@
 'use client';
+
 import Products from '@/components/common/Buyer/Products';
-import { getDoc, collection, doc, query, where, getDocs } from 'firebase/firestore';
-import { db, auth } from '@/lib/firebase/client';
-import useSWR from 'swr';
-import Loader from '@/components/common/Loader';
-import Error from '@/components/common/Error';
-const getFavCategory = async () => {
-  let querySnapshot;
-  if (!auth.currentUser) {
-    querySnapshot = await getDocs(collection(db, 'categories'));
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { useCategories } from '@/hooks/useCategories';
+import { Category } from '@/components/common/CategoryDropdown';
 
-    let categories: any = [];
-    querySnapshot.forEach((doc) => {
-      categories.push({
-        id: doc.id,
-        name: doc.data().title,
-        subCategories: doc.data().list,
-        lifeSpan: doc.data().lifeSpaan,
-        image: doc.data().image,
-        slug: doc.data().title,
-        href: '/products?category'
-      });
-    });
-    return categories;
-  }
-  const userDocSnapshot = await getDoc(doc(db, 'users', auth.currentUser?.uid));
-
-  if (!userDocSnapshot.exists()) return;
-  const userDoc = userDocSnapshot.data();
-
-  if (userDoc?.favourites) {
-    querySnapshot = await getDocs(
-      query(collection(db, 'categories'), where('title', 'in', userDoc.favourites))
-    );
-  } else {
-    querySnapshot = await getDocs(collection(db, 'categories'));
-  }
-
-  let categories: any = [];
-  querySnapshot.forEach((doc) => {
-    categories.push({
-      id: doc.id,
-      name: doc.data().title,
-      subCategories: doc.data().list,
-      lifeSpan: doc.data().lifeSpaan,
-      image: doc.data().image,
-      slug: doc.data().title,
+const getCategoriesList = (categories: any) => {
+  let list: any = categories?.map((c: any) => {
+    return {
+      id: c.id,
+      name: c.title,
+      subCategories: c.list,
+      lifeSpan: c.lifeSpaan,
+      image: c.image,
+      slug: c.title,
       href: '/products?category'
-    });
+    };
   });
-  return categories;
+  return list;
 };
-
 export default function Index() {
-  const { data: categories, error, isLoading } = useSWR('forYouCategories', getFavCategory);
-
-  if (isLoading) return <Loader className="w-full h-[70vh] flex items-center justify-center" />;
-
-  if (error) return <Error className="w-full h-[70vh] flex items-center justify-center" />;
-
-  return <Products foryou categories={categories} />;
+  const { user } = useCurrentUser();
+  const { categories: allCategories } = useCategories();
+  let categories = getCategoriesList(allCategories);
+  const filteredCategories =
+    user?.favourites?.length > 0
+      ? categories.filter((c: Category) => user.favourites.includes(c.name))
+      : categories;
+  return <Products foryou categories={filteredCategories} />;
 }
