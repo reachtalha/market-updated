4d6aef14377f8d17b39fec234ed71bb9b8df9b17
dev5 <dev5@ccript.com>
2023-08-28 20:16:13 +0500

    add formatDateOrTime formatter

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -25,3 +25,25 @@ export function formatMonth(seconds: any) {
   });
   return formatter.format(new Date(seconds));
 }
+
+export function formatDateOrTime(date: Date): string {
+  const currentDate = new Date();
+
+  if (
+    date.getDate() === currentDate.getDate() &&
+    date.getMonth() === currentDate.getMonth() &&
+    date.getFullYear() === currentDate.getFullYear()
+  ) {
+    const timeDifference = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 60 * 60));
+
+    if (timeDifference < 1) {
+      const minutesDifference = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 60));
+      return `${minutesDifference} min ago`;
+    } else {
+      return `${timeDifference} hr ago`;
+    }
+  } else {
+    const dayNameOptions: Intl.DateTimeFormatOptions = { weekday: 'long' };
+    return date.toLocaleDateString('en-US', dayNameOptions);
+  }
+}
