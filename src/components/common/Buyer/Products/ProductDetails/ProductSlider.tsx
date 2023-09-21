fc181fc16d809f46c56da67fdc837faf7b74db74
Hanan Munir <hananmunir471gmail.com>
2023-09-21 19:07:50 +0500

    Added sharing options

--- src/components/common/Buyer/Products/ProductDetails/ProductSlider.tsx
+++ src/components/common/Buyer/Products/ProductDetails/ProductSlider.tsx
@@ -2,27 +2,58 @@ import Image from 'next/image';
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
 import { Keyboard, Pagination } from 'swiper/modules';
 import { Share2 } from 'lucide-react';
-
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { toast } from 'react-hot-toast';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { FacebookShareButton, TwitterShareButton } from 'react-share';
 
 export default function ProductSlider({ images }: { images: string[] }) {
   return (
     <Swiper
       loop
       pagination={{ clickable: true }}
-      keyboard 
+      keyboard
       modules={[Keyboard, Pagination]}
       className="container w-full h-full mySwiper relative"
     >
       <div className="absolute z-10 top-5 right-5">
-        <a
-          href={`https://web.whatsapp.com/send?text=Check out this amazing product on All Organics ${window.location.href}`}
-          data-action="share/whatsapp/share"
-          target="_blank"
-        >
-          <Share2 />
-        </a>
+        <Popover>
+          <PopoverTrigger>
+            <Share2 />
+          </PopoverTrigger>
+          <PopoverContent className="w-56">
+            <div className="flex flex-col items-center justify-center">
+              <a
+                href={`https://web.whatsapp.com/send?text=Check out this amazing product on All Organics ${window.location.href}`}
+                data-action="share/whatsapp/share"
+                target="_blank"
+                className="hover:bg-slate-200 w-full p-2 flex items-center justify-center rounded-lg"
+              >
+                Share On Whatsapp
+              </a>
+              <span className="hover:bg-slate-200 w-full p-2 flex items-center justify-center rounded-lg">
+                <FacebookShareButton url={encodeURI(window.location.href)}>
+                  Share On Facebook
+                </FacebookShareButton>
+              </span>
+              <span className="hover:bg-slate-200 w-full p-2 flex items-center justify-center rounded-lg">
+                <TwitterShareButton url={encodeURI(window.location.href)}>
+                  Share On Twitter
+                </TwitterShareButton>
+              </span>
+              <span
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                  toast.success('Link Copied');
+                }}
+                className="hover:bg-slate-200 w-full p-2 cursor-pointer flex items-center justify-center rounded-lg"
+              >
+                Copy Link
+              </span>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
       {images.map((url: string, index: number) => (
         <SwiperSlide className=" w-full h-full" key={index}>
